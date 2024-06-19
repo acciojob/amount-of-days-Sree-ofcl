480af8d6e1916d51f59f5491780e585d3614ab2c
Sree-ofcl <157797249+Sree-ofcl@users.noreply.github.com>
2024-06-19 18:35:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function daysOfAYear(num) {
+	if(num%4 === 0 && num%100 !===0 || num%400 === 0)
+	{
+		return 366;
+	}
+	else{
+		return 365;
+	}
+}
+daysOfAYear(1990)
+daysOfAYear(2024)
